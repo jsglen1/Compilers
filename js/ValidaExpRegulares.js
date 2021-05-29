88d06104aba56a9e82a826a75f5fea089f507c8c
jsglen1 <wtfdxd@gmail.com>
2021-05-28 20:33:16 -0500

    v2.0

--- js/ValidaExpRegulares.js
+++ js/ValidaExpRegulares.js
@@ -1,4 +1,12 @@
-ValidaExpresion = /(^[A-Z]\d{3}[a-zA-Z]{3}\W{3}$)/g
+ValidaExpresion = /(^[a-zA-Z]{3}\W{2}\d{5}\W{2}$)/g
+
+VARIABLEvALIDA = / lo que encesitas /
+
+ValidaExpresion.test(data);
+
+/* 
+aAa$$98304##
+*/
 
 /* a.	Una Mayúscula seguida   de 3 números luego seguida de 3 caracteres y luego de 
 tres caracteres especiales como se muestra en el siguiente ejemplo:
